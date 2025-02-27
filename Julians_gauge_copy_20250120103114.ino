93445c5b23d55b377c8aabdbf4303be0f4da426e
Andy Doswell <andydoswell@hotmail.com>
2025-02-27 21:37:35 +0000

    Update Julians_gauge_copy_20250120103114.ino
    increased accuracy

--- Julians_gauge_copy_20250120103114.ino
+++ Julians_gauge_copy_20250120103114.ino
@@ -1,5 +1,5 @@
 /* Tank in on A0, Fed from a 200 ohm resistor from the 5V rail.
- A 2N7000 FET is employed to drive the gauge. It's gate it fed from A0, via a 100 ohm resistor. The gauge output is connected from the drain via a 4.7 ohm resistor
+ A 2N7000 FET is employed to drive the gauge. It's gate is fed from A0, via a 100 ohm resistor. The gauge output is connected from the drain via a 4.7 ohm resistor
  The FET source is connected to GND
 */
 int raw;
@@ -7,20 +7,20 @@ uint32_t value;
 int calZero = 10;
 float calSpan = 0.8888;
 void setup() {
-  Serial.begin(9600);
+  pinMode(A0, INPUT);
 }
 
 void loop() {
-  for (int i = 0; i < 100; i++) {
-    value = value + analogRead(A0);
+  for (int i = 0; i <= 99; i++) {
+    value += analogRead(A0);
     delay(1);
   }
-  raw = value / 100;
-  float volts = (raw * 5.0) / 1024.0;
-  float resistance = (volts * 200) / (5 / volts) + calZero * calSpan;
-  float tankPercent = 103 + -0.905 * resistance + (0.00197 * pow(resistance, 2));
-  int PWM = (120 + 1.05 * tankPercent) + (0.00229 * pow(tankPercent, 2));
+
+  value = value / 100;
+  float volts = (value * 5.0) / 1024.0;
+  float r = (volts * 200) / (5 - volts);
+  float tankPercent = 104 + -0.94 * r + (2.76E-3 * pow(r, 2)) + (-2.95E-6 * pow(r, 3));
+  int PWM = (120 + -5.24E-3 * tankPercent) + (0.0311 * pow(tankPercent, 2)) + (-1.97E-4 * pow(tankPercent, 3));
+  PWM = constrain(PWM, 0, 255);
   analogWrite(3, PWM);
-  value = 0;
-  delay(100);
 }
